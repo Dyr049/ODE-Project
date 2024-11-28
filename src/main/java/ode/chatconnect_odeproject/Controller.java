f85dbb89ce07f248a56483ac2fb030d8c5cdacc0
Diyar <celikdiyar4943@gmail.com>
2024-11-28 15:16:59 +0100

    Login Design gefertigt

--- src/main/java/ode/chatconnect_odeproject/Controller.java
+++ src/main/java/ode/chatconnect_odeproject/Controller.java
@@ -5,8 +5,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class Controller {
+
+    @FXML
+    public AnchorPane anchorPane_right;
+
+    @FXML
+    public AnchorPane anchorPane_left;
 
     @FXML
     private Button btn_anmelden;
--- src/main/java/ode/chatconnect_odeproject/Main.java
+++ src/main/java/ode/chatconnect_odeproject/Main.java
@@ -6,18 +6,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
         stage.setTitle("Chat Connect - Login/Registration");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+
         launch();
     }
 }
